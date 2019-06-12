10f8ace1b200c207aedcfc8029400467aa42d592
mizraim17 <mizraim.martinez@gmail.com>
2019-06-11 23:36:08 -0500

    se agrego la funcionalidad para las peticiones get y delete

--- routes/product.js
+++ routes/product.js
@@ -4,7 +4,7 @@ const Product =  require('../models/product-model');
 const router = express.Router();
 
 router.post("/product",(req,res,next) => {
-  console.log("----------------------",req.body)
+  console.log("request ",req.body)
   const   {
     name, price
   }= req.body;
@@ -16,14 +16,41 @@ router.post("/product",(req,res,next) => {
 
   product.save()
     .then(response => {
-      console.log('response.data',res.json(response.data))
+      console.log('response.data')
       res.json(response)
     })
     .catch((err)=>{
       console.log("error",err)
     })
-
 });
+
+  router.get("/product",(req,res,next)=>{
+    console.log('**********-*************')
+    Product.find()
+      .then((result)=>{
+        console.log('entro al chid0')
+        res.json(result)
+      })
+      .catch(err => next(err));
+  })
+
+  router.delete("/product/:id",(req,res,next)=>{
+    console.log("entro")
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      res.status(400).json({ message: "El id no existe" });
+      return;
+    }
+    Product.findByIdAndDelete(req.params.id)
+      .then(() => {
+        res.json({
+          message: `Proyecto con id ${req.params.id} se ha borrado correctamente`
+        });
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  })
+
 
 
 
